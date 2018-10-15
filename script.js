7efea28f1f0683f5f3136267c1c7a4cbba6e7dca
Ruhi Tamanna Alam <ruhialamsithy@gmail.com>
2018-10-15 03:14:13 +0000

    task 2c,4a

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ console.log(heading.innerText);
 heading.innerText="change it";
 /* Task 2 using plain JavaScript */
 
+//var test=1;
+
 var textBtn = document.getElementById('addText2a');
 textBtn.addEventListener('click',function(event){
     var task2=document.getElementById('task2a');
     var paragraph= document.createElement('p');
     task2.innerText="hello world";
     task2.appendChild(paragraph);
+   // test=3;
     
 });
-
+ //console.log(test);
 function changeBGcolor(event){
     var body= document.getElementsByTagName('body')[0];
     if (event.target.innerText=="Red") {
@@ -26,6 +29,14 @@ function changeBGcolor(event){
     
 }
 
-
+function redirectToGoogle(event){
+    window.location.assign("http://www.google.com")
+}
 
 /* Task 4 using jQuery */
+
+ $('#addText4').click(function(event){
+    var paragraph=$('<p>').text('Hello World');
+    $('#task4a').append(paragraph);
+    
+})
